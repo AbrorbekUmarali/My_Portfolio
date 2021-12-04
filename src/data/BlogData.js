ddc9a1ed9919fa432565238dd3a93cf78cbe65dd
AbrorbekUmarali <umaralievabror_1994@mail.ru>
2021-12-04 18:34:04 +0100

    last commit

---
+++ src/data/BlogData.js
@@ -0,0 +1,29 @@
+export const Blogs = [
+  {
+    id: 1,
+    name: "How to build a Fabulous Todo App using React, Redux and Framer-Motion",
+    tags: ["react", "redux", "framer-motion"],
+    date: "26 May, 2021",
+    imgSrc:
+      "https://res.cloudinary.com/practicaldev/image/fetch/s--r5oUDhhT--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/036s7j5b73up7455rdth.png",
+    link: "https://dev.to/codebucks/build-redux-react-todo-list-app-with-animations-using-framer-motion-1mp1",
+  },
+  {
+    id: 2,
+    name: "A Covid-19 tracker app built with React JS, Material UI, React Hooks and Chart JS",
+    tags: ["react", "hooks", "chart.js"],
+    date: "12 June, 2021",
+    imgSrc:
+      "https://reactjsexample.com/content/images/2021/11/Covid-19-tracker-app.jpg",
+    link: "https://covid-insights.netlify.app/",
+  },
+  {
+    id: 3,
+    name: "dynamic and responsive E-Commerce ",
+    tags: ["react.js", "hooks", "redux"],
+    date: "11 October, 2021",
+    imgSrc:
+      "https://github.com/AlmogWer/furniture-e-commerce/raw/master/img/Capture2.PNG",
+    link: "https://almogwer-furniture-ecommerce.netlify.app/",
+  },
+];
---
+++ src/data/WorkData.js
@@ -0,0 +1,32 @@
+export const Work = [
+  {
+    id: 1,
+    name: "Job listing website",
+    description:
+      "A Job listing website build in ReactJS. With searching and filtering functionalities.",
+    tags: ["react", "sass"],
+
+    demo: "http://react-job-listing-website.codebucks27.vercel.app/",
+    github: "https://github.com/AbrorbekUmarali/Job_List",
+  },
+
+  {
+    id: 2,
+    name: "Todo App(React + Redux)",
+    description:
+      "A todo app build using Redux-toolkit and framer-motion for smooth animations.",
+    tags: ["react", "redux", "framer-motion"],
+
+    demo: "http://react-redux-todo-app-git-main-codebucks27.vercel.app/",
+    github: "https://github.com/AbrorbekUmarali/TodoWithRedux",
+  },
+  {
+    id: 3,
+    name: "A Covid-19 tracker app",
+    description:
+      "A Covid-19 tracker app built with React JS, Material UI, React Hooks and Chart JS",
+    tags: ["react", "hooks"],
+    demo: "https://covid-insights.netlify.app/",
+    github: "https://github.com/AbrorbekUmarali/Covid_19",
+  },
+];
